c7070fcfe3e0bbb26777f6938098884bafb70922
redglasses67 <kizakazu6067@gmail.com>
2020-12-20 01:41:36 +0900

    UvAnimPostprocess 多少汎用性あるように修正

--- UnityScript/Editor/UvAnimPostprocess.cs
+++ UnityScript/Editor/UvAnimPostprocess.cs
@@ -1,12 +1,89 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System.Linq;
 
 public class UvAnimPostprocess : AssetPostprocessor
 {
+	void OnPreprocessModel()
+	{
+		var modelImporter = assetImporter as ModelImporter;
+		if (modelImporter == null) { return; }
+
+		var isChanged     = false;
+
+		if (modelImporter.extraUserProperties.IsNullOrEmpty() == false)
+		{
+			var beforeExUserPropList = modelImporter.extraUserProperties.ToList();
+			// Tex もしくは Map と付くカスタムアトリビュートを一旦全部削除する
+			beforeExUserPropList.RemoveAll(p => p.Contains("Tex") || p.Contains("Map"));
+			modelImporter.extraUserProperties = beforeExUserPropList.ToArray();
+		}
+
+		if (isChanged)
+		{
+			EditorUtility.SetDirty(modelImporter);
+		}
+	}
+
+	void OnPostprocessGameObjectWithUserProperties(
+		GameObject importedGameObject,
+		string[] propNames,
+		System.Object[] values)
+	{
+		var modelImporter  = assetImporter as ModelImporter;
+		if (modelImporter == null) { return; }
+
+		var exUserPropList = modelImporter.extraUserProperties.ToList();
+		var isChanged      = false;
+
+		for (int i = 0; i < propNames.Length; i++)
+		{
+			var propName  = propNames[i];
+			var propValue = values[i] as string;
+
+			// カスタムアトリビュート名が TargetTexProp から始まっていなければパス
+			if (propName.StartsWith("TargetTexProp") == false) { continue; }
+			// カスタムアトリビュート値に Tex もしくは Map がついてなければパス
+			if (propValue.Contains("Tex") == false && propValue.Contains("Map") == false) { continue; }
+
+			exUserPropList.Add(propValue + "___" + importedGameObject.name);
+			isChanged = true;
+		}
+
+		if (isChanged)
+		{
+			modelImporter.extraUserProperties = exUserPropList.ToArray();
+			EditorUtility.SetDirty(modelImporter);
+		}
+	}
+
 	void OnPostprocessGameObjectWithAnimatedUserProperties(
 		GameObject importedGameObject,
 		EditorCurveBinding[] bindings)
 	{
+		var modelImporter  = assetImporter as ModelImporter;
+		if (modelImporter == null) { return; }
+
+		var exUserPropList = modelImporter.extraUserProperties.ToList();
+		var targetPropName = "_MainTex";
+		foreach (var exUserProp in exUserPropList)
+		{
+			// exUserProp の末尾に importedGameObject名 がなければパス
+			if (exUserProp.EndsWith(importedGameObject.name) == false) { continue; }
+
+			var underIndexPos = exUserProp.LastIndexOf("___");
+			// exUserProp に ___ がなければパス
+			if (underIndexPos == -1) { continue; }
+
+			var targetTexProp = exUserProp.Substring(0, underIndexPos);
+			targetPropName = targetTexProp;
+			break;
+		}
+
+		var importedTrans = importedGameObject.transform;
+		// ルートの Transform からの相対パスを取得できる（ルートは含まれない）
+		var relativePath  = AnimationUtility.CalculateTransformPath(importedTrans, importedTrans.root);
+
 		for (var i = 0; i < bindings.Length; i++)
 		{
 			var propName = bindings[i].propertyName;
@@ -19,27 +96,25 @@ public class UvAnimPostprocess : AssetPostprocessor
 				continue;
 			}
 
-			Debug.Log("PropName = " + propName + " : type = " + bindings[i].type + " : path = " + bindings[i].path);
-
-			bindings[i].path = "";
+			bindings[i].path = relativePath;
 			bindings[i].type = typeof(MeshRenderer);
 			// Legacy パイプラインですとメインテクスチャーのプロパティ名は _MainTex ですが、
 			// URP では _BaseMap : HDRP では _BaseColorMap となっているので注意（ Unityさん、なぜ統一しない… ）
 			if (propName.StartsWith("uvRepeatAnimU"))
 			{
-				bindings[i].propertyName = "material._MainTex_ST.x";
+				bindings[i].propertyName = "material." + targetPropName + "_ST.x";
 			}
 			if (propName.StartsWith("uvRepeatAnimV"))
 			{
-				bindings[i].propertyName = "material._MainTex_ST.y";
+				bindings[i].propertyName = "material." + targetPropName + "_ST.y";
 			}
 			if (propName.StartsWith("uvOffsetAnimU"))
 			{
-				bindings[i].propertyName = "material._MainTex_ST.z";
+				bindings[i].propertyName = "material." + targetPropName + "_ST.z";
 			}
 			if (propName.StartsWith("uvOffsetAnimV"))
 			{
-				bindings[i].propertyName = "material._MainTex_ST.w";
+				bindings[i].propertyName = "material." + targetPropName + "_ST.w";
 			}
 		}
 	}
